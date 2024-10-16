028785ea9f4b9f133f15027420fd7cf5fa84fc9a
Martin Gil <martinyfede2004@gmail.com>
2024-10-15 21:38:06 -0300

    Model, mapper, repository, service y controller de Localidad.

--- Back/app/Models/Localidad.php
+++ Back/app/Models/Localidad.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Localidad extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id_localidad', 'localidad'];
+    protected $table = 'localidad';
+    protected $primaryKey = 'id_localidad';
+
+    public $timestamps = false; 
+
+    // Una localidad Pertenece a un administrador
+    public function administrador():BelongsTo{
+        return $this->belongsTo(Administrador::class, 'id_localidad', 'id_localidad');
+    }
+    // Una localidad pertenece a un docente
+    public function Docente():BelongsTo{
+        return $this->belongsTo(Docente::class, 'id_localidad', 'id_localidad');
+    }
+
+    // Una localidad pertenece a un alumno
+    public function alumno():BelongsTo{
+        return $this->belongsTo(Alumno::class, 'id_localidad', 'id_localidad');
+    }
+
 }
